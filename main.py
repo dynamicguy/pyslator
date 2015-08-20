1d0148c14e07eb8e85aae6bdda084de2fa8649dc
Nurul Ferdous <nurul@ferdo.us>
2015-08-21 02:14:14 +0600

    added google translate api

--- main.py
+++ main.py
@@ -1,29 +1,66 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# !/usr/bin/env python
+
+"""Simple command-line tool for Translate.
+Command-line application that translates some text.
+"""
+from __future__ import print_function
+
+__author__ = 'nurul@ferdous (Nurul Ferdous)'
+
+import json
+import argparse
+from googleapiclient.discovery import build
 
 
-__author__ = 'ferdous'
+def call_api(q, src_lang, target_lang):
+    service = build('translate', 'v2', developerKey='CHANGE_ME')
+    print(service.translations().list(
+        source=src_lang,
+        target=target_lang,
+        q=q
+    ).execute())
 
-import optparse
-import os
-from os.path import join, getsize
+
+def read_data(d):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            read_data(v)
+        else:
+            # print(v)
+            print("{0} => {1}".format(k, v))
+            # call_api(v)
 
 
-def run(folder):
-    print('reading folder: ' + folder)
-    for root, dirs, files in os.walk(folder):
-        print(root, "consumes", end=" ")
-        print(sum(getsize(join(root, name)) for name in files), end=" ")
-        print("bytes in", len(files), "non-directory files")
-        for file in files:
-            body = open(folder + '/' + file).readline()
-            print(body)
+def do_translate(args):
+    print('reading folder: ' + args.file)
+    data = json.load(open(args.file))
+    read_data(data)
+
+
+def run(args):
+    answer = do_translate(args)
+    if args.quiet:
+        print(answer)
+    elif args.verbose:
+        print("{} to the power {} equals {}".format(args.file, args.lang, answer))
+    else:
+        print("{}^{} == {}".format(args.file, args.lang, answer))
 
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser()
-    parser.add_option("-d", "--directory", metavar="DIR", help="Directory to scan for image files")
+    parser = argparse.ArgumentParser(description="calculate X to the power of Y")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+    group.add_argument("-q", "--quiet", action="store_true", help="run quietly")
+    parser.add_argument("file", type=str, help="file that need to be translated")
+    parser.add_argument("lang", type=str, help="to which language it need to translated")
+    args = parser.parse_args()
 
-    opts, args = parser.parse_args()
-    print('scanning directory: ', opts.directory)
-    run(opts.directory)
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("file", type=str, help="file that need to be translated")
+    # parser.add_argument("lang", type=str, help="to which language it need to translated")
+    # parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+    # args = parser.parse_args()
+    run(args)
